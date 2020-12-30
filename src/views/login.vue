338c31f33145e4922e32f3337640f6782f96652b
zhuxingjie <1163258992@qq.com>
2020-12-30 17:37:17 +0800

    重写登录界面样式

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,59 +1,84 @@
 <template>
   <div class="home_bg">
     <div class="login_content">
-      <div class="title">
-        <!-- <img class="logo" src="../assets/images/logo.png" alt /> -->
+      <div class="wel-img">
+        <img class="logo" src="../assets/images/wel.jpg" alt />
       </div>
-      <div class="input_content">
-        <div>
-          <label style="width:100%">
-            <span>用户名：</span>
-            <input
-              @focus="curFocus=0"
-              @blur="curFocus=-1"
-              type="text"
-              v-model.trim="loginForm.name"
-            />
-            <div :style="{width:curFocus===0?'100%':''}" class="line"></div>
-          </label>
+      <div class="input-box">
+        <div class="logo">
+          <img src="../assets/images/log.png" alt="" />
         </div>
+        <div class="input_content">
+          <div>
+            <label style="width: 100%">
+              <span>用户名</span>
+              <input
+                @focus="curFocus = 0"
+                @blur="curFocus = -1"
+                type="text"
+                v-model.trim="loginForm.name"
+              />
+              <div
+                :style="{ width: curFocus === 0 ? '100%' : '' }"
+                class="line"
+              ></div>
+            </label>
+          </div>
 
-        <div>
-          <label style="width:100%">
-            <span>密码：</span>
-            <input
-              @focus="curFocus=1"
-              @blur="curFocus=-1"
-              type="password"
-              v-model.trim="loginForm.password"
-            />
-            <div :style="{width:curFocus===1?'100%':''}" class="line"></div>
-          </label>
+          <div>
+            <label style="width: 100%">
+              <span>密码</span>
+              <input
+                @focus="curFocus = 1"
+                @blur="curFocus = -1"
+                type="password"
+                v-model.trim="loginForm.password"
+              />
+              <div
+                :style="{ width: curFocus === 1 ? '100%' : '' }"
+                class="line"
+              ></div>
+            </label>
+          </div>
+          <div class="code-area">
+            <label>
+              <span>验证码</span>
+              <input
+                @focus="curFocus = 2"
+                @blur="curFocus = -1"
+                type="text"
+                v-model.trim="loginForm.code"
+              />
+              <div
+                :style="{ width: curFocus === 2 ? '100%' : '' }"
+                class="line"
+              ></div>
+            </label>
+            <div
+              class="code"
+              :style="{
+                backgroundImage: `url(${codeUrl})`,
+                backgroundSize: '100% 100%',
+              }"
+              @click="getCode"
+            ></div>
+          </div>
         </div>
-        <div class="code-area">
-          <label>
-            <span>验证码：</span>
-            <input
-              @focus="curFocus=2"
-              @blur="curFocus=-1"
-              type="text"
-              v-model.trim="loginForm.code"
-            />
-            <div :style="{width:curFocus===2?'100%':''}" class="line"></div>
-          </label>
+        <div class="footer">
           <div
-            class="code"
-            :style="{backgroundImage:`url(${codeUrl})`,backgroundSize:'100% 100%'}"
-            @click="getCode"
-          ></div>
+            :class="{ login_btn: true, is_loading: loading }"
+            @click="loginFn"
+          >
+            {{ loading ? "正在登录" : "登录" }}
+          </div>
         </div>
       </div>
-      <div class="footer">
-        <div :class="{login_btn:true,is_loading:loading}" @click="loginFn">{{loading?'正在登录':'登录'}}</div>
-      </div>
+
       <div class="copy">
-        <p style="padding:0;margin:0">—— 途咪小视频 后台管理系统 ——</p>
-        <span style="white-space:nowrap">Copyright 2020 © 途咪智慧视频有限技术公司</span>
+        <p style="padding: 0; margin: 0">—— 途咪VLOG 后台管理系统 ——</p>
+        <span style="white-space: nowrap"
+          >Copyright 2020 © 途咪智慧视频有限技术公司</span
+        >
       </div>
     </div>
   </div>
@@ -99,7 +124,8 @@ export default {
     async loginFn() {
       if (this.loading) return;
       if (!this.loginForm.name) return this.$message.warning("请输入用户名！");
-      if (!this.loginForm.password) return this.$message.warning("请输入密码！");
+      if (!this.loginForm.password)
+        return this.$message.warning("请输入密码！");
       if (!this.loginForm.code) return this.$message.warning("请输入验证码！");
 
       // 登录actions
@@ -140,27 +166,74 @@ export default {
 <style scoped lang="less">
 .home_bg {
   height: 100vh;
-  background-image: url("../assets/images/gs.png");
-  background-repeat: repeat;
+  background: url("../assets/images/bg.jpg") center no-repeat;
+  background-size:cover;
+  // filter: blur(10px);
   display: flex;
   justify-content: center;
   align-items: center;
   overflow: hidden;
 
+  &::after {
+    content: "";
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    position: absolute;
+    z-index: 2;
+    background: rgba(0, 0,0, 0.6);
+  }
+
   .login_content {
     position: relative;
-    width: 500px;
-    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
-    border-radius: 8px;
+    display: flex;
+    z-index: 3;
+    width: 770px;
+   box-shadow: 3px 5px 18px 0px rgba(88, 82, 70, 0.45);
     background: rgba(255, 255, 255, 1);
 
+    .wel-img {
+      position: relative;
+      flex-shrink: 0;
+      width: 50%;
+      cursor:pointer;
+
+      img {
+        // filter: blur(px);
+        position: absolute;
+        top: -10%;
+        right: -15px;
+        box-shadow: 8px 13px 29px 0px rgba(29, 30, 23, 0.86); 
+      }
+    }
+
+    .input-box {
+      padding: 30px 45px 0 70px;
+      box-sizing: border-box;
+
+      .logo {
+        display: flex;
+        justify-content: center;
+        margin-bottom:20px;
+        
+        img{
+          width:160px;
+          height:42px;
+        }
+      }
+    }
+
     .input_content {
-      padding: 0 80px 0;
+      color: #5c544c;
+      font-weight:bold;
+      font-family: '宋体';
 
       input {
         width: 100%;
         height: 32px;
         font-size: 16px;
+        
         border: none;
         outline: none;
       }
@@ -172,7 +245,9 @@ export default {
         left: 50%;
         transform: translateX(-50%);
         height: 2px;
-        background: #5a84fd;
+        // background: rgb(244,186,27);
+        background:#5c544c;
+        // background:#000;
         transition: 0.5s;
       }
     }
@@ -194,12 +269,28 @@ export default {
     .footer {
       box-sizing: border-box;
       width: 100%;
-      padding: 60px 80px;
+      padding:20px 6px 50px;
+
+      .login_btn {
+        border: 1px solid #5c544c;
+        font-size: 14px;
+        padding: 14px 0;
+        text-align: center;
+        width: 100%;
+        transition: 0.5s;
+        cursor: pointer;
+
+        &:hover {
+          background:#5c544c;
+        //   border: 1px solid #d8a312;
+          color:#fff;
+        }
+      }
     }
 
     .copy {
-      position: absolute;
-      bottom: -80px;
+      position: fixed;
+      bottom: 80px;
       text-align: center;
       left: 50%;
       transform: translateX(-50%);
@@ -213,7 +304,7 @@ export default {
   position: relative;
   display: flex;
   padding-top: 28px;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  border-bottom: 1px solid #5c544c;
 }
 
 .input_content > div > span {
@@ -222,7 +313,7 @@ export default {
   min-width: 60px;
   align-items: center;
   font-size: 14px;
-  color: rgba(0, 0, 0, 0.54);
+  color: #5c544c;
 }
 
 .code {
@@ -233,23 +324,6 @@ export default {
   align-items: center;
   justify-content: center;
   cursor: pointer;
-}
-
-.login_btn {
-  background: #5a84fd;
-  box-shadow: 0 5px 30px rgba(0, 66, 255, 0.3);
-  font-size: 14px;
-  padding: 10px 0;
-  color: white;
-  text-align: center;
-  width: 100%;
-  border-radius: 5px;
-  transition: 0.5s;
-  cursor: pointer;
-
-  &:hover {
-    background: rgba(0, 66, 255, 0.8);
-  }
 }
 
 .is_loading {
